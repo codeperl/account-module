0343626d80dbf4ee54015c51869102d18580b210
codeperl <it.codeperl@gmail.com>
2019-05-22 13:14:10 +0600

    Fixing config.php

--- Config/config.php
+++ Config/config.php
@@ -1,5 +1,24 @@
 <?php
 
 return [
-    'name' => 'Account'
+    'name' => 'Account',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+    */
+
+    'providers' => [
+        /*
+         * Package Service Providers...
+         */
+        Spatie\Permission\PermissionServiceProvider::class,
+
+    ],
 ];
