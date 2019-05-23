e5df283122f1a06cd29b567dd870dc2c8d578ebd
codeperl <it.codeperl@gmail.com>
2019-05-23 11:42:16 +0600

    Fixing AccountServiceProvider class.

--- AccountServiceProvider.php
+++ AccountServiceProvider.php
@@ -1,9 +1,13 @@
 <?php
 
-namespace Spatie\Permission;
+namespace Modules\Account;
 
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Class AccountServiceProvider
+ * @package Modules\Account
+ */
 class AccountServiceProvider extends ServiceProvider
 {
     public function boot()
