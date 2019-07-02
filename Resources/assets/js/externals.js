9396293977dc88aa3238f624126da82dada96cba
codeperl <it.codeperl@gmail.com>
2019-07-02 16:24:33 +0600

    updating account js.

--- Resources/assets/js/externals.js
+++ Resources/assets/js/externals.js
@@ -41,10 +41,6 @@ $(document).ready(function() {
         $(this).tab('show');
     });
 
-    $(".delete").on("submit", function () {
-        return confirm("Do you want to delete this item?");
-    });
-
     $('#generate-resources').on('click', function(e) {
         e.preventDefault();
         var $this = $(this);
